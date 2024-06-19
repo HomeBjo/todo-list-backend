b9ebe1182db57f57a910f99cfb0670934875cefb
HomeBjo <lala2-wuppertal@hotmail.com>
2024-06-19 16:50:01 +0200

    $*

--- todolist/serializers.py
+++ todolist/serializers.py
@@ -5,3 +5,11 @@ class TodoItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoItem
         fields = '__all__'
+        extra_kwargs = {
+            'author': {'required': False}  # Macht das author-Feld nicht erforderlich
+        }
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        validated_data['author'] = request.user
+        return super().create(validated_data)
--- todolist/views.py
+++ todolist/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render
-
-
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-
 from rest_framework.views import APIView
-
 from todolist.models import TodoItem
 from todolist.serializers import TodoItemSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 class TodoItemView(APIView):
-    # authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = []
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     
     def get(self, request, format=None):
-      
-        todos = TodoItem.objects.all()
+        todos = TodoItem.objects.filter(author=request.user)
         serializer = TodoItemSerializer(todos, many=True)
         return Response(serializer.data)
+    
+    
+    def post(self, request, format=None):
+        print(f"Authenticated user: {request.user}") 
+        serializer = TodoItemSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)  
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    
 class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
